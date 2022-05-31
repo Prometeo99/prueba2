65fc341242469ef185c831ae212b0c1353d0b106
Prometeo99 <106593431+Prometeo99@users.noreply.github.com>
2022-05-31 08:08:55 -0300

    Add files via upload

---
+++ Clase_20220404/Clase_20220404/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Clase_20220404
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ejercicios e = new Ejercicios();
+
+            //e.Guia1Ej4A();
+            //e.Guia1Ej9SinBucle();
+            //e.Guia1Ej9ConBucle();
+            //e.Guia1Ej10();
+            e.Guia2Ej3();
+            //e.Guia2Ej5();
+            //e.Guia2Ej6();
+
+            Console.WriteLine("Presione enter para salir...");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Clase_20220404/Ejercicios.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_20220404
+{
+    internal class Ejercicios
+    {
+        public void Guia1Ej4A()
+        {
+            int materiasTotales;
+            int materiasAprobadas;
+            int materiasFaltantes;
+
+            Console.Write("Por favor ingrese el total de las materias de la carrera: ");
+            materiasTotales = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Por favor ingrese el total de las materias aprobadas: ");
+            materiasAprobadas = Convert.ToInt32(Console.ReadLine());
+
+            materiasFaltantes = materiasTotales - materiasAprobadas;
+
+            Console.WriteLine("A usted le faltan " + materiasFaltantes + " materias para recibirse.");
+
+        }
+
+        public void Guia1Ej9SinBucle()
+        {
+            float acumulador = 0;
+
+            Console.Write("Por favor ingrese un numero: ");
+            acumulador += Convert.ToSingle(Console.ReadLine());
+            Console.Write("Por favor ingrese un numero: ");
+            acumulador += Convert.ToSingle(Console.ReadLine());
+            Console.Write("Por favor ingrese un numero: ");
+            acumulador += Convert.ToSingle(Console.ReadLine());
+            Console.Write("Por favor ingrese un numero: ");
+            acumulador += Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("La sumatoria de esos numeros es: " + acumulador);
+        }
+
+        public void Guia1Ej9ConBucle()
+        {
+            float acumulador = 0;
+            int contador = 0;
+
+            do
+            {
+                Console.Write("Por favor ingrese un numero: ");
+                acumulador += Convert.ToSingle(Console.ReadLine());
+                contador++;
+
+            } while (contador < 4);
+
+            Console.WriteLine("La sumatoria de esos numeros es: " + acumulador);
+
+        }
+
+        public void Guia1Ej10()
+        {
+            float materiasAprobadas;
+            float materiasRendidas;
+            float promedio;
+            float ranking;
+
+            Console.Write("Por favor ingrese la cantidad de la materias aprobadas: ");
+            materiasAprobadas = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Por favor ingrese la cantidad de la materias rendidas: ");
+            materiasRendidas = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Por favor ingrese su promedio: ");
+            promedio = Convert.ToSingle(Console.ReadLine());
+
+            ranking = (materiasAprobadas / materiasRendidas) * 100 + materiasAprobadas * 3 + promedio * 3;
+            Console.WriteLine("El ranking del alumno es: " + ranking);
+        }
+
+        public void Guia2Ej3()
+        {
+            string opcion;
+            bool flagDatoErroneo;
+
+            do
+            {
+                flagDatoErroneo = false;
+                Console.Write("Hola, quiere permanecer en el programa? (S/N): ");
+                opcion = Console.ReadLine();
+
+                if (opcion != "S" && opcion != "N")
+                {
+                    Console.WriteLine("Usted seleccionó un dato erróneo.");
+                    flagDatoErroneo = true;
+                }
+
+            } while (opcion == "S" || flagDatoErroneo == true);
+
+            Console.WriteLine("Hasta luego!");
+        }
+
+        public void Guia2Ej5()
+        {
+            string opcion;
+            string mensaje;
+            bool flagDatoErroneo;
+
+            do
+            {
+                flagDatoErroneo = false;
+                Console.WriteLine("Seleccione la operación a realizar:" + System.Environment.NewLine +
+                "1 - Altas" + System.Environment.NewLine +
+                "2 - Bajas" + System.Environment.NewLine +
+                "3 - Modificaciones" + System.Environment.NewLine +
+                "4 - Salir");
+
+                opcion = Console.ReadLine();
+
+                if (opcion == "1")
+                {
+                    mensaje = "Usted seleccinó Altas";
+                }
+                else if (opcion == "2")
+                {
+                    mensaje = "Usted seleccionó Bajas";
+                }
+                else if (opcion == "3")
+                {
+                    mensaje = "Usted seleccionó Modificaciones";
+                }
+                else if (opcion == "4")
+                {
+                    mensaje = "Usted seleccionó Salir";
+                }
+                else
+                {
+                    mensaje = "Usted ingresó un dato erróneo. Deberá ingresarlo nuevamente.";
+                    flagDatoErroneo = true;
+                }
+
+                Console.WriteLine(mensaje);
+            } while (flagDatoErroneo == true); // while (flagDatoErroneo);
+
+        }
+
+        public void Guia2Ej6()
+        {
+            int numero1;
+            int numero2;
+            string mensaje;
+
+            Console.Write("Por favor ingrese el primer numero: ");
+            numero1 =  Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Por favor ingrese el segundo numero: ");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+
+            if (numero1 == numero2)
+            {
+                mensaje = "Los numeros ingresados son iguales";
+            }
+            else
+            {
+                mensaje = "Los numeros ingresados son distintos";
+            }
+
+            Console.WriteLine(mensaje);
+        }
+    }
+}
